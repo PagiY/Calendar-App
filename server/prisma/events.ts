49ac960df1425ee87771cd61fcf23e2613ecc278
Paula Yap <pagiyap003@gmail.com>
2024-01-04 22:49:34 +0800

    🛠️ Fixed prisma schema

--- server/prisma/events.ts
+++ server/prisma/events.ts
@@ -1,16 +1,16 @@
 export const events = [
   {
     id: '1',
-    endDate: '2023-12-01T05:00:00.000Z',
-    startDate: '2023-11-29T16:00:00.000Z',
+    end: '2023-12-01T05:00:00.000Z',
+    start: '2023-11-29T16:00:00.000Z',
     title: 'Test1',
     allDay: false,
     description: 'Lorem Ipsum Dolor Sit Amet',
   },
   {
     id: '2',
-    endDate: '2023-12-28T16:00:00.000Z',
-    startDate: '2023-12-27T16:00:00.000Z',
+    end: '2023-12-28T16:00:00.000Z',
+    start: '2023-12-27T16:00:00.000Z',
     title: 'Test2-Allday',
     allDay: true,
     description: 'Lorem Ipsum Dolor Sit Amet',
