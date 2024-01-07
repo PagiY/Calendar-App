7b53e0ef00b5e2e4e52559e978931e9e92fbb2d7
Paula Yap <pagiyap003@gmail.com>
2024-01-07 21:44:01 +0800

    Added dockerfile, dockerignore, modified configs

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -4,10 +4,16 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'plugin:react/recommended',
+    // 'plugin:react/recommended',
     'airbnb',
     'airbnb-typescript',
+    // 'airbnb/hooks',
+    // 'plugin:react/jsx-runtime',
+    // 'plugin:@typescript-eslint/recommended',
+    // 'plugin:react-hooks/recommended',
+    // 'plugin:@tanstack/eslint-plugin-query/recommended',
   ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     // ecmaFeatures: {
@@ -15,7 +21,8 @@ module.exports = {
     // },
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './client/tsconfig.json',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
   },
   plugins: [
     'react',
@@ -32,6 +39,16 @@ module.exports = {
     ],
     'import/prefer-default-export': [
       'off',
+    ],
+    'import/extensions': ['error',
+      'ignorePackages',
+      {
+        '': 'never',
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
     ],
   },
 };
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url))},
+    ]
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: 'http://0.0.0.0:8080',
+  }
 })
